7904a895aefa4233369e229f6d4218bcd3f92cbd
Yashsharma009 <116294789+Yashsharma009@users.noreply.github.com>
2023-05-31 22:40:40 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name='text-generation-app',
     version='1.0',
     description='Text Generation Streamlit App',
-    author='Your Name',
-    author_email='your@email.com',
+    author='yash',
+    author_email='sharyash1101@gmail.com',
     packages=[''],
     install_requires=[
         'streamlit==0.87.0',
         'transformers==4.11.3',
     ],
 )
-
