a5090c03c58acd7cfc1e8a38930895092aaab5fd
Yashsharma009 <116294789+Yashsharma009@users.noreply.github.com>
2023-05-31 22:34:04 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+from setuptools import setup
+
+setup(
+    name='text-generation-app',
+    version='1.0',
+    description='Text Generation Streamlit App',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=[''],
+    install_requires=[
+        'streamlit==0.87.0',
+        'transformers==4.11.3',
+    ],
+)
+
